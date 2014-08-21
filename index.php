a37c6dda6468c310f22b83e4a98c69279990a042
vegizinho <miroslav.vegh@techlab.hr>
2014-08-21 16:37:34 +0200

    Front page layout change

--- index.php
+++ index.php
@@ -84,10 +84,8 @@
       </ol>
       <div class="carousel-inner">
         <div class="item active">
-        
           <img src="images/slides/slide1.jpg" alt="First slide">
-          <!-- <img src="data:image/gif;base64,R0lGODlhAQABAIAAAHd3dwAAACH5BAAAAAAALAAAAAABAAEAAAICRAEAOw==" alt="First slide"> -->
-          
+          <!-- <img src="data:image/gif;base64,R0lGODlhAQABAIAAAHd3dwAAACH5BAAAAAAALAAAAAABAAEAAAICRAEAOw==" alt="First slide"> -->   
           <div class="container">
             <div class="carousel-caption">
               <h1>Example headline.</h1>
@@ -97,7 +95,8 @@
           </div>
         </div>
         <div class="item">
-          <img src="data:image/gif;base64,R0lGODlhAQABAIAAAGZmZgAAACH5BAAAAAAALAAAAAABAAEAAAICRAEAOw==" alt="Second slide">
+        	<img src="images/slides/slide1.jpg" alt="Second slide">
+			<!-- <img src="data:image/gif;base64,R0lGODlhAQABAIAAAGZmZgAAACH5BAAAAAAALAAAAAABAAEAAAICRAEAOw==" alt="Second slide"> -->
           <div class="container">
             <div class="carousel-caption">
               <h1>Another example headline.</h1>
@@ -107,7 +106,8 @@
           </div>
         </div>
         <div class="item">
-          <img src="data:image/gif;base64,R0lGODlhAQABAIAAAFVVVQAAACH5BAAAAAAALAAAAAABAAEAAAICRAEAOw==" alt="Third slide">
+        	<img src="images/slides/slide1.jpg" alt="Third slide">
+			<!-- <img src="data:image/gif;base64,R0lGODlhAQABAIAAAFVVVQAAACH5BAAAAAAALAAAAAABAAEAAAICRAEAOw==" alt="Third slide"> -->
           <div class="container">
             <div class="carousel-caption">
               <h1>One more for good measure.</h1>
@@ -131,32 +131,21 @@
 
       <!-- Three columns of text below the carousel -->
       <div class="row">
-        <div class="col-lg-3">
-          <img class="img-circle" src="data:image/gif;base64,R0lGODlhAQABAIAAAHd3dwAAACH5BAAAAAAALAAAAAABAAEAAAICRAEAOw==" alt="Generic placeholder image" style="width: 140px; height: 140px;">
-          <h2>Heading</h2>
-          <p>Donec sed odio dui. Etiam porta sem malesuada magna mollis euismod. Nullam id dolor id nibh ultricies vehicula ut id elit. Morbi leo risus, porta ac consectetur ac, vestibulum at eros. Praesent commodo cursus magna.</p>
-          <p><a class="btn btn-default" href="#" role="button">View details &raquo;</a></p>
+        <div class="col-md-3">
+          <img src="images/slides/t1.jpg" alt="Audiocinema-art" style="width: 250px; height: 250px;">
         </div><!-- /.col-lg-3 -->
-        <div class="col-lg-3">
-          <img class="img-circle" src="data:image/gif;base64,R0lGODlhAQABAIAAAHd3dwAAACH5BAAAAAAALAAAAAABAAEAAAICRAEAOw==" alt="Generic placeholder image" style="width: 140px; height: 140px;">
-          <h2>Heading</h2>
-          <p>Duis mollis, est non commodo luctus, nisi erat porttitor ligula, eget lacinia odio sem nec elit. Cras mattis consectetur purus sit amet fermentum. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh.</p>
-          <p><a class="btn btn-default" href="#" role="button">View details &raquo;</a></p>
+        <div class="col-md-3">
+          <img src="images/slides/t2.jpg" alt="Audiocinema-art" style="width: 250px; height: 250px;">
         </div><!-- /.col-lg-3 -->
-        <div class="col-lg-3">
-          <img class="img-circle" src="data:image/gif;base64,R0lGODlhAQABAIAAAHd3dwAAACH5BAAAAAAALAAAAAABAAEAAAICRAEAOw==" alt="Generic placeholder image" style="width: 140px; height: 140px;">
-          <h2>Heading</h2>
-          <p>Donec sed odio dui. Cras justo odio, dapibus ac facilisis in, egestas eget quam. Vestibulum id ligula porta felis euismod semper. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus.</p>
-          <p><a class="btn btn-default" href="#" role="button">View details &raquo;</a></p>
+        <div class="col-md-3">
+          <img src="images/slides/t3.jpg" alt="Audiocinema-art" style="width: 250px; height: 250px;">
         </div><!-- /.col-lg-3 -->
-        <div class="col-lg-3">
-          <img class="img-circle" src="data:image/gif;base64,R0lGODlhAQABAIAAAHd3dwAAACH5BAAAAAAALAAAAAABAAEAAAICRAEAOw==" alt="Generic placeholder image" style="width: 140px; height: 140px;">
-          <h2>Heading</h2>
-          <p>Donec sed odio dui. Cras justo odio, dapibus ac facilisis in, egestas eget quam. Vestibulum id ligula porta felis euismod semper. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus.</p>
-          <p><a class="btn btn-default" href="#" role="button">View details &raquo;</a></p>
+        <div class="col-md-3">
+          <img src="images/slides/t4.jpg" alt="Audiocinema-art" style="width: 250px; height: 250px;">
         </div><!-- /.col-lg-3 -->
       </div><!-- /.row -->
 
+      <div class="row subcontent"></div>
 
       <!-- START THE FEATURETTES -->
 
@@ -164,14 +153,22 @@
 
       <!-- /END THE FEATURETTES -->
 
+    </div><!-- /.container -->
+    
+          
+      <div class="overfooter">
+      	<h2>Ovo je zavrsna aplikacija po cijeloj sirini, da se izjednaci s karuselom</h2>
+      	<p>Ovdje mozemo staviti nesto teksta, facebook like box, twitter box, prijavu na newsletter i slican sadrzaj, kao i pregled glavnih 
+      	linkova nekim manjim sitnijim fontom.</p>
+      </div>
 
       <!-- FOOTER -->
+      <div class="container">
       <footer>
         <p class="pull-right"><a href="#">Back to top</a></p>
         <p>&copy; 2014 Company, Inc. &middot; <a href="#">Privacy</a> &middot; <a href="#">Terms</a></p>
       </footer>
-
-    </div><!-- /.container -->
+      </div>
 
 
     <!-- Bootstrap core JavaScript
