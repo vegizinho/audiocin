5c334da8d34bee1bd04aa557b89292508e8364ce
vegizinho <miroslav.vegh@techlab.hr>
2014-10-13 19:05:17 +0200

    Acaweb new layout

--- index.php
+++ index.php
@@ -30,130 +30,73 @@
     <link href="css/carousel.css" rel="stylesheet">
     <link href="css/main.css" rel="stylesheet">
   </head>
-<!-- NAVBAR
-================================================== -->
+
   <body>
-    <div class="navbar-wrapper">
-      <div class="container">
+  
+  		<div class="row">
+	  		<div class="topmenu">
+	  			<div class="col-md-3">LOGO</div>
+	  			<div class="col-md-3 pullRight">TRGOVINE | O NAMA | SERVISI</div>
+	  			<div class="col-md-3 pullLeft">STORES | ABOUT US | SERVICES</div>
+	  			<div class="col-md-3">TRAZILICA</div>
+	  		</div>
+  		</div>
 
-        <div class="navbar navbar-inverse navbar-static-top" role="navigation">
-          <div class="container">
-            <div class="navbar-header">
-              <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
-                <span class="sr-only">Toggle navigation</span>
-                <span class="icon-bar"></span>
-                <span class="icon-bar"></span>
-                <span class="icon-bar"></span>
-              </button>
-              <a class="navbar-brand" href="#">Prototype 1</a>
-            </div>
-            <div class="navbar-collapse collapse">
-              <ul class="nav navbar-nav">
-                <li class="active"><a href="#">Home</a></li>
-                <li><a href="#about">About</a></li>
-                <li><a href="#contact">Contact</a></li>
-                <li class="dropdown">
-                  <a href="#" class="dropdown-toggle" data-toggle="dropdown">Dropdown <span class="caret"></span></a>
-                  <ul class="dropdown-menu" role="menu">
-                    <li><a href="#">Action</a></li>
-                    <li><a href="#">Another action</a></li>
-                    <li><a href="#">Something else here</a></li>
-                    <li class="divider"></li>
-                    <li class="dropdown-header">Nav header</li>
-                    <li><a href="#">Separated link</a></li>
-                    <li><a href="#">One more separated link</a></li>
-                  </ul>
-                </li>
-              </ul>
-            </div>
-          </div>
-        </div>
-
-      </div>
-    </div>
-
-
-    <!-- Carousel
-    ================================================== -->
-    <div id="myCarousel" class="carousel slide" data-ride="carousel">
-      <!-- Indicators -->
-      <ol class="carousel-indicators">
-        <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
-        <li data-target="#myCarousel" data-slide-to="1"></li>
-        <li data-target="#myCarousel" data-slide-to="2"></li>
-      </ol>
-      <div class="carousel-inner">
-        <div class="item active">
-          <img src="images/slides/slide1.jpg" alt="First slide">
-          <!-- <img src="data:image/gif;base64,R0lGODlhAQABAIAAAHd3dwAAACH5BAAAAAAALAAAAAABAAEAAAICRAEAOw==" alt="First slide"> -->   
-          <div class="container">
-            <div class="carousel-caption">
-              <h1>Example headline.</h1>
-              <p>Note: If you're viewing this page via a <code>file://</code> URL, the "next" and "previous" Glyphicon buttons on the left and right might not load/display properly due to web browser security rules.</p>
-              <p><a class="btn btn-lg btn-primary" href="#" role="button">Sign up today</a></p>
-            </div>
-          </div>
-        </div>
-        <div class="item">
-        	<img src="images/slides/slide1.jpg" alt="Second slide">
-			<!-- <img src="data:image/gif;base64,R0lGODlhAQABAIAAAGZmZgAAACH5BAAAAAAALAAAAAABAAEAAAICRAEAOw==" alt="Second slide"> -->
-          <div class="container">
-            <div class="carousel-caption">
-              <h1>Another example headline.</h1>
-              <p>Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus. Nullam id dolor id nibh ultricies vehicula ut id elit.</p>
-              <p><a class="btn btn-lg btn-primary" href="#" role="button">Learn more</a></p>
-            </div>
-          </div>
-        </div>
-        <div class="item">
-        	<img src="images/slides/slide1.jpg" alt="Third slide">
-			<!-- <img src="data:image/gif;base64,R0lGODlhAQABAIAAAFVVVQAAACH5BAAAAAAALAAAAAABAAEAAAICRAEAOw==" alt="Third slide"> -->
-          <div class="container">
-            <div class="carousel-caption">
-              <h1>One more for good measure.</h1>
-              <p>Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus. Nullam id dolor id nibh ultricies vehicula ut id elit.</p>
-              <p><a class="btn btn-lg btn-primary" href="#" role="button">Browse gallery</a></p>
-            </div>
-          </div>
-        </div>
-      </div>
-      <a class="left carousel-control" href="#myCarousel" role="button" data-slide="prev"><span class="glyphicon glyphicon-chevron-left"></span></a>
-      <a class="right carousel-control" href="#myCarousel" role="button" data-slide="next"><span class="glyphicon glyphicon-chevron-right"></span></a>
-    </div><!-- /.carousel -->
-
-
-
-    <!-- Marketing messaging and featurettes
-    ================================================== -->
-    <!-- Wrap the rest of the page in another container to center all the content. -->
+	<div id="carousel-generic" class="carousel slide" data-ride="carousel">
+	  <!-- Indicators -->
+	  <ol class="carousel-indicators">
+	    <li data-target="#carousel-generic" data-slide-to="0" class="active"></li>
+	    <li data-target="#carousel-generic" data-slide-to="1"></li>
+	    <li data-target="#carousel-generic" data-slide-to="2"></li>
+	  </ol>
+	
+	  <!-- Wrapper for slides -->
+	  <div class="carousel-inner">
+	    <div class="item active">
+	      <a href="link1"><img src="images/slides/slide1920.jpg" alt="Alt text"></a>
+	    </div>
+	
+	    <div class="item">
+	      <a href="link2"><img src="images/slides/slide1920.jpg" alt="Alt text"></a>
+	    </div>
+	
+	    <div class="item">
+	      <a href="link3"><img src="images/slides/slide1920.jpg" alt="Alt text"></a>
+	    </div>
+	  </div>
+	
+	  <!-- Controls -->
+	  <a class="left carousel-control" href="#carousel-generic" data-slide="prev">
+	    <span class="glyphicon glyphicon-chevron-left"></span>
+	  </a>
+	  <a class="right carousel-control" href="#carousel-generic" data-slide="next">
+	    <span class="glyphicon glyphicon-chevron-right"></span>
+	  </a>
+	</div>
 
     <div class="container marketing">
 
       <!-- Three columns of text below the carousel -->
-      <div class="row">
-        <div class="col-md-3">
-          <img src="images/slides/t1.jpg" alt="Audiocinema-art" style="width: 250px; height: 250px;">
-        </div><!-- /.col-lg-3 -->
-        <div class="col-md-3">
-          <img src="images/slides/t2.jpg" alt="Audiocinema-art" style="width: 250px; height: 250px;">
-        </div><!-- /.col-lg-3 -->
-        <div class="col-md-3">
-          <img src="images/slides/t3.jpg" alt="Audiocinema-art" style="width: 250px; height: 250px;">
-        </div><!-- /.col-lg-3 -->
-        <div class="col-md-3">
-          <img src="images/slides/t4.jpg" alt="Audiocinema-art" style="width: 250px; height: 250px;">
-        </div><!-- /.col-lg-3 -->
-      </div><!-- /.row -->
+      <div class="row moveDown">
+        <div class="col-md-6">
+          <a href="#link1"><img class="img-responsive" src="images/2-4 HE.jpg" alt="Audiocinema-art"></a>
+        </div>
+        <div class="col-md-6">
+          <<a href="#link1"><img class="img-responsive" src="images/2-4 HE.jpg" alt="Audiocinema-art"></a>
+        </div>
+        </div>
+       <div class="row">
+        <div class="col-md-6">
+          <a href="#link1"><img class="img-responsive" src="images/3-4 MG.jpg" alt="Audiocinema-art"></a>
+        </div>
+        <div class="col-md-6">
+          <a href="#link1"><img class="img-responsive" src="images/4-4 C.jpg" alt="Audiocinema-art"></a>
+        </div>
+      </div>
 
       <div class="row subcontent"></div>
 
-      <!-- START THE FEATURETTES -->
-
-      
-
-      <!-- /END THE FEATURETTES -->
-
-    </div><!-- /.container -->
+    </div>
     
           
       <div class="overfooter">
