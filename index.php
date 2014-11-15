1e4c20aaa5ef108d671057ae90832b416d4bffb4
vegizinho <miroslav.vegh@techlab.hr>
2014-11-15 13:55:23 +0100

    New carousel images: Acaweb-bootstrap-frontpage
    Task-Url:

--- index.php
+++ index.php
@@ -1,136 +1,286 @@
 <!DOCTYPE html>
 <html lang="en">
-  <head>
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <meta name="description" content="">
-    <meta name="author" content="">
-    <link rel="icon" href="favicon.ico">
+<head>
+<meta charset="utf-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<meta name="description" content="">
+<meta name="author" content="">
+<link rel="icon" href="favicon.ico">
 
-    <title>Carousel Template for Bootstrap</title>
+<title>Carousel Template for Bootstrap</title>
 
-    <!-- Bootstrap core CSS -->
-    <link href="bootstrap-3.2.0-dist/css/bootstrap.min.css" rel="stylesheet">
+<!-- Bootstrap core CSS -->
+<link href="bootstrap-3.2.0-dist/css/bootstrap.min.css" rel="stylesheet">
 
-    <!-- Just for debugging purposes. Don't actually copy these 2 lines! -->
-    <!--[if lt IE 9]><script src="../../assets/js/ie8-responsive-file-warning.js"></script><![endif]-->
-    <script src="assets/js/ie-emulation-modes-warning.js"></script>
+<!-- Just for debugging purposes. Don't actually copy these 2 lines! -->
+<!--[if lt IE 9]><script src="../../assets/js/ie8-responsive-file-warning.js"></script><![endif]-->
+<script src="assets/js/ie-emulation-modes-warning.js"></script>
 
-    <!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
-    <script src="assets/js/ie10-viewport-bug-workaround.js"></script>
+<!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
+<script src="assets/js/ie10-viewport-bug-workaround.js"></script>
 
-    <!-- HTML5 shim and Respond.js IE8 support of HTML5 elements and media queries -->
-    <!--[if lt IE 9]>
+<!-- HTML5 shim and Respond.js IE8 support of HTML5 elements and media queries -->
+<!--[if lt IE 9]>
       <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
       <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
     <![endif]-->
 
-    <!-- Custom styles for this template -->
-    <link href="css/carousel.css" rel="stylesheet">
-    <link href="css/main.css" rel="stylesheet">
-  </head>
+<!-- Custom styles for this template -->
+<link href="css/carousel.css" rel="stylesheet">
+<link href="css/main.css" rel="stylesheet">
+</head>
 
-  <body>
-  
-	 <div class="topmenu">
+<body>
 
-	  		<div class="col-md-3">LOGO</div>
-	  		<div class="col-md-3 pullRight">TRGOVINE | O NAMA | SERVISI</div>
-	  		<div class="col-md-3 pullLeft">STORES | ABOUT US | SERVICES</div>
-	  		<div class="col-md-3">TRAZILICA</div>
+	<div class="topmenu">
 
-  	</div>
+		<div class="col-md-3">LOGO</div>
+		<div class="col-md-3 pullRight">TRGOVINE | O NAMA | SERVISI</div>
+		<div class="col-md-3 pullLeft">STORES | ABOUT US | SERVICES</div>
+		<div class="col-md-3">TRAZILICA</div>
 
-	<div id="carousel-generic" class="carousel slide" data-ride="carousel">
-	  <!-- Indicators -->
-	  <ol class="carousel-indicators">
-	    <li data-target="#carousel-generic" data-slide-to="0" class="active"></li>
-	    <li data-target="#carousel-generic" data-slide-to="1"></li>
-	    <li data-target="#carousel-generic" data-slide-to="2"></li>
-	  </ol>
-	
-	  <!-- Wrapper for slides -->
-	  <div class="carousel-inner">
-	    <div class="item active">
-	      <a href="link1"><img src="images/slides/slide1920.jpg" alt="Alt text"></a>
-	    </div>
-	
-	    <div class="item">
-	      <a href="link2"><img src="images/slides/slide1920.jpg" alt="Alt text"></a>
-	    </div>
-	
-	    <div class="item">
-	      <a href="link3"><img src="images/slides/slide1920.jpg" alt="Alt text"></a>
-	    </div>
-	  </div>
-	
-	  <!-- Controls -->
-	  <a class="left carousel-control" href="#carousel-generic" data-slide="prev">
-	    <span class="glyphicon glyphicon-chevron-left"></span>
-	  </a>
-	  <a class="right carousel-control" href="#carousel-generic" data-slide="next">
-	    <span class="glyphicon glyphicon-chevron-right"></span>
-	  </a>
 	</div>
 
-    <div class="container marketing">
+	<div id="carousel-generic" class="carousel slide" data-ride="carousel">
+		<!-- Indicators -->
+		<ol class="carousel-indicators">
+			<li data-target="#carousel-generic" data-slide-to="0" class="active"></li>
+			<li data-target="#carousel-generic" data-slide-to="1"></li>
+			<li data-target="#carousel-generic" data-slide-to="2"></li>
+			<li data-target="#carousel-generic" data-slide-to="3"></li>
+			<li data-target="#carousel-generic" data-slide-to="4"></li>
+			<li data-target="#carousel-generic" data-slide-to="5"></li>
+			<li data-target="#carousel-generic" data-slide-to="6"></li>
+			<li data-target="#carousel-generic" data-slide-to="7"></li>
+			<li data-target="#carousel-generic" data-slide-to="8"></li>
+			<li data-target="#carousel-generic" data-slide-to="9"></li>
+			<li data-target="#carousel-generic" data-slide-to="10"></li>
+			<li data-target="#carousel-generic" data-slide-to="11"></li>
+			<li data-target="#carousel-generic" data-slide-to="12"></li>
+			<li data-target="#carousel-generic" data-slide-to="13"></li>
+			<li data-target="#carousel-generic" data-slide-to="14"></li>
+			<li data-target="#carousel-generic" data-slide-to="15"></li>
+			<li data-target="#carousel-generic" data-slide-to="16"></li>
+			<li data-target="#carousel-generic" data-slide-to="17"></li>
+			<li data-target="#carousel-generic" data-slide-to="18"></li>
+			<li data-target="#carousel-generic" data-slide-to="19"></li>
+			<li data-target="#carousel-generic" data-slide-to="20"></li>
 
-      <!-- Three columns of text below the carousel -->
-      <div class="row moveDown">
-      	<div class="col-md-2"></div>
-	      <div class="col-md-8 cent-approx">
-	          <a href="#link1"><img class="img-responsive" src="images/1-4 AV.jpg" alt="Audiocinema-art"></a>
-	      </div>
-	     <div class="col-md-2"></div>
-      </div>
-      <div class="row moveDown">
-      	<div class="col-md-2"></div>
-	      <div class="col-md-8 cent-approx">
-	          <a href="#link1"><img class="img-responsive" src="images/2-4 HE.jpg" alt="Audiocinema-art"></a>
-	      </div>
-	     <div class="col-md-2"></div>
-      </div>
-      <div class="row moveDown">
-      	<div class="col-md-2"></div>
-	      <div class="col-md-8 cent-approx">
-	          <a href="#link1"><img class="img-responsive" src="images/3-4 MG.jpg" alt="Audiocinema-art"></a>
-	      </div>
-	      <div class="col-md-2"></div>
-      </div>
-      <div class="row moveDown">
-      	<div class="col-md-2"></div>
-	      <div class="col-md-8 cent-approx">
-	          <a href="#link1"><img class="img-responsive" src="images/4-4 C.jpg" alt="Audiocinema-art"></a>
-	      </div>
-	     <div class="col-md-2"></div>
-      </div>
+		</ol>
 
-      <div class="row subcontent"></div>
+		<!-- Wrapper for slides -->
+		<div class="carousel-inner">
 
-    </div>
-    
-          
-      <div class="overfooter">
-      	<h2>Ovo je zavrsna aplikacija po cijeloj sirini, da se izjednaci s karuselom</h2>
-      	<p>Ovdje mozemo staviti nesto teksta, facebook like box, twitter box, prijavu na newsletter i slican sadrzaj, kao i pregled glavnih 
-      	linkova nekim manjim sitnijim fontom.</p>
-      </div>
+			<div class="item active">
+				<a
+					href="http://www.audiocinema-art.hr/index.php?qs=kef-LS50-ponuda-u-listopadu-i-studenom"><img
+					src="images/slides/car01-KEF-LS50.jpg" alt="Alt text"> </a>
+			</div>
 
-      <!-- FOOTER -->
-      <div class="container">
-      <footer>
-        <p class="pull-right"><a href="#">Back to top</a></p>
-        <p>&copy; 2014 Company, Inc. &middot; <a href="#">Privacy</a> &middot; <a href="#">Terms</a></p>
-      </footer>
-      </div>
+			<div class="item">
+				<a
+					href="http://www.audiocinema-art.hr/index.php?qs=nad&ty=AUDIO-VIDEO%20SUSTAVI&grupa=prikaziAkcije"><img
+					src="images/slides/car02-akcija 2014-06 Monitor CA - AV.jpg" alt="Alt text"> </a>
+			</div>
+
+			<div class="item">
+				<a
+					href="http://www.audiocinema-art.hr/index.php?qs=akcija-triangle-zvucnici-neponovljiva"><img
+					src="images/slides/car03-akcija 2014-05 Triangle.jpg" alt="Alt text"> </a>
+			</div>
+
+			<div class="item">
+				<a
+					href="http://www.audiocinema-art.hr/index.php?qs=audio-video-sustavi&ty=AUDIO-VIDEO%20SUSTAVI&grupa=prikazi&manufacturer=Monitor%20Audio&ak=34"><img
+					src="images/slides/car04-MA Silver.jpg" alt="Alt text"> </a>
+			</div>
+
+			<div class="item">
+				<a
+					href="http://www.audiocinema-art.hr/index.php?qs=kef-premijera-na-hes-2014"><img
+					src="images/slides/car05-KEF Blade 2.jpg" alt="Alt text"> </a>
+			</div>
+
+			<div class="item">
+				<a
+					href="http://www.audiocinema-art.hr/index.php?qs=cambridge-audio-streameri"><img
+					src="images/slides/car06-CA streameri.jpg" alt="Alt text"> </a>
+			</div>
+
+			<div class="item">
+				<a
+					href="http://www.audiocinema-art.hr/index.php?qs=akcija-2014-04%20do-70posto"><img
+					src="images/slides/car07-akcija 2014-11 50do 70posto.jpg" alt="Alt text"> </a>
+			</div>
+
+			<div class="item">
+				<a
+					href="http://www.audiocinema-art.hr/index.php?qs=nova-teac-501-serija"><img
+					src="images/slides/car08-Teac 501.jpg" alt="Alt text"> </a>
+			</div>
+
+			<div class="item">
+				<a
+					href="http://www.audiocinema-art.hr/index.php?qs=monitor-audio-silver&ty=AUDIO-VIDEO%20SUSTAVI&grupa=prikazi&manufacturer=Pro-Ject&ak=34"><img
+					src="images/slides/car09-Project Sig10.jpg" alt="Alt text"> </a>
+			</div>
+
+			<div class="item">
+				<a
+					href="http://www.audiocinema-art.hr/index.php?qs=high-end-audio&ty=HIGH%20END%20AUDIO&grupa=prikazi&manufacturer=Crystal%20Cable&ak=35"><img
+					src="images/slides/car10-crystal.jpg" alt="Alt text"> </a>
+			</div>
+
+			<div class="item">
+				<a
+					href="http://www.audiocinema-art.hr/index.php?qs=high-end-audio&ty=HIGH%20END%20AUDIO&grupa=prikazi&manufacturer=Dan%20D%C2%B4Agostino&ak=35"><img
+					src="images/slides/car11-Dagostino.jpg" alt="Alt text"> </a>
+			</div>
+
+			<div class="item">
+				<a
+					href="http://www.audiocinema-art.hr/index.php?qs=high-end-audio&ty=HIGH%20END%20AUDIO&grupa=prikazi&manufacturer=McIntosh&ak=35"><img
+					src="images/slides/car12-McIntosh.jpg" alt="Alt text"> </a>
+			</div>
+
+			<div class="item">
+				<a href="http://www.audiocinema-art.hr/index.php?qs=vivid-audio"><img
+					src="images/slides/car13-VividG3.jpg" alt="Alt text"> </a>
+			</div>
+
+			<div class="item">
+				<a
+					href="http://www.audiocinema-art.hr/index.php?qs=high-end-audio&ty=HIGH%20END%20AUDIO&grupa=prikazi&manufacturer=VTL&ak=35"><img
+					src="images/slides/car14-VTL.jpg" alt="Alt text"> </a>
+			</div>
+
+			<div class="item">
+				<a
+					href="http://www.audiocinema-art.hr/index.php?qs=high-end-audio&ty=HIGH%20END%20AUDIO&grupa=prikazi&manufacturer=Wilson%20Audio&ak=35"><img
+					src="images/slides/car15-Wilson.jpg" alt="Alt text"> </a>
+			</div>
+
+			<div class="item">
+				<a
+					href="http://www.audiocinema-art.hr/index.php?qs=gadget-audio-video&ty=GADGET&grupa=prikazi&manufacturer=Geneva%20Lab&ak=36"><img
+					src="images/slides/car16-Geneva.jpg" alt="Alt text"> </a>
+			</div>
+
+			<div class="item">
+				<a
+					href="http://www.audiocinema-art.hr/index.php?qs=gadget-audio-video&ty=GADGET&grupa=prikazi&manufacturer=Scandyna&ak=36"><img
+					src="images/slides/car17-Scandyna.jpg" alt="Alt text"> </a>
+			</div>
+
+			<div class="item">
+				<a
+					href="http://www.audiocinema-art.hr/index.php?qs=spanjolska-multimedija"><img
+					src="images/slides/car18-Eis.jpg" alt="Alt text"> </a>
+			</div>
+
+			<div class="item">
+				<a
+					href="http://www.audiocinema-art.hr/index.php?qs=salamander-vrhunsko-sjedenje"><img
+					src="images/slides/car19-Salamander.jpg" alt="Alt text"> </a>
+			</div>
+
+			<div class="item">
+				<a
+					href="http://www.audiocinema-art.hr/index.php?qs=salamander-namjestaj&ty=MULTIROOM&grupa=prikazi&manufacturer=Sim%202&ak=33"><img
+					src="images/slides/car20-SIm2.jpg" alt="Alt text"> </a>
+			</div>
+
+			<div class="item">
+				<a
+					href="http://www.audiocinema-art.hr/index.php?qs=salamander-namjestaj"><img
+					src="images/slides/car21-SIm2.jpg" alt="Alt text"> </a>
+			</div>
 
 
-    <!-- Bootstrap core JavaScript
+
+
+
+
+		</div>
+
+		<!-- Controls -->
+		<a class="left carousel-control" href="#carousel-generic"
+			data-slide="prev"> <span class="glyphicon glyphicon-chevron-left"></span>
+		</a> <a class="right carousel-control" href="#carousel-generic"
+			data-slide="next"> <span class="glyphicon glyphicon-chevron-right"></span>
+		</a>
+	</div>
+
+	<div class="container marketing">
+
+		<!-- Three columns of text below the carousel -->
+		<div class="row moveDown">
+			<div class="col-md-2"></div>
+			<div class="col-md-8 cent-approx">
+				<a href="#link1"><img class="img-responsive" src="images/1-4 AV.jpg"
+					alt="Audiocinema-art"> </a>
+			</div>
+			<div class="col-md-2"></div>
+		</div>
+		<div class="row moveDown">
+			<div class="col-md-2"></div>
+			<div class="col-md-8 cent-approx">
+				<a href="#link1"><img class="img-responsive" src="images/2-4 HE.jpg"
+					alt="Audiocinema-art"> </a>
+			</div>
+			<div class="col-md-2"></div>
+		</div>
+		<div class="row moveDown">
+			<div class="col-md-2"></div>
+			<div class="col-md-8 cent-approx">
+				<a href="#link1"><img class="img-responsive" src="images/3-4 MG.jpg"
+					alt="Audiocinema-art"> </a>
+			</div>
+			<div class="col-md-2"></div>
+		</div>
+		<div class="row moveDown">
+			<div class="col-md-2"></div>
+			<div class="col-md-8 cent-approx">
+				<a href="#link1"><img class="img-responsive" src="images/4-4 C.jpg"
+					alt="Audiocinema-art"> </a>
+			</div>
+			<div class="col-md-2"></div>
+		</div>
+
+		<div class="row subcontent"></div>
+
+	</div>
+
+
+	<div class="overfooter">
+		<h2>Ovo je zavrsna aplikacija po cijeloj sirini, da se izjednaci s
+			karuselom</h2>
+		<p>Ovdje mozemo staviti nesto teksta, facebook like box, twitter box,
+			prijavu na newsletter i slican sadrzaj, kao i pregled glavnih linkova
+			nekim manjim sitnijim fontom.</p>
+	</div>
+
+	<!-- FOOTER -->
+	<div class="container">
+		<footer>
+			<p class="pull-right">
+				<a href="#">Back to top</a>
+			</p>
+			<p>
+				&copy; 2014 Company, Inc. &middot; <a href="#">Privacy</a> &middot;
+				<a href="#">Terms</a>
+			</p>
+		</footer>
+	</div>
+
+
+	<!-- Bootstrap core JavaScript
     ================================================== -->
-    <!-- Placed at the end of the document so the pages load faster -->
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
-    <script src="bootstrap-3.2.0-dist/js/bootstrap.min.js"></script>
-    <script src="bootstrap-3.2.0-dist/assets/js/docs.min.js"></script>
-  </body>
+	<!-- Placed at the end of the document so the pages load faster -->
+	<script
+		src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+	<script src="bootstrap-3.2.0-dist/js/bootstrap.min.js"></script>
+	<script src="bootstrap-3.2.0-dist/assets/js/docs.min.js"></script>
+</body>
 </html>
